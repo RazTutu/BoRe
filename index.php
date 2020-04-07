e9b7043ac41ba820c8bf66b2103be01ee9c93887
AndreiPatrascan <45433079+AndreiPatrascan@users.noreply.github.com>
2020-04-07 18:28:13 +0300

    Added books, changed the layout, added a bit of responsivness

--- index.php
+++ index.php
@@ -32,91 +32,241 @@
     </header>
     <main class="content" id="page">
 
-    
-    <section  class="orizontalSection">
-        <div class = "loginElement">      
-        <img src="https://picsum.photos/1700/200" class= "info">    
-        </div>
-        <div class = "loginElement" id = "loginSection">
-            <p id = "login__title">
-                Welcome Back
-            </p>
-            <p id = "login__message">
-                Don't miss your next best read. Sign in to stay updated.
-            </p>
-            <button type="button" id="popup-login">Login</button>   
-            <p id = "login__register_message">
-                Not registered? Click here:
-            </p>       
-            <button type="button" id="popup-register">Register</button>
-        </div>
-    </section>
 
-    <section  class="orizontalSection">
-        <div class="statistics">      
-          <p class="statistics_text">
-              Statistics
-          </p>
-        </div>
-        <div>
-            <img src="https://picsum.photos/1700/200" class= "info">
-        </div>
-    </section>
+    <section class = "mainLayout">
 
-    <div>
-        <p id="writinglogin">
-            Latest books: 
-        <p>
-    </div>
+      <section class = "verticalSection">
 
-    <section  class="orizontalSection">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        <img src="https://picsum.photos/200/200" class= "info">
-        
-    </section>
+        <section  class="orizontalSection">
 
+            <div class = "loginElement">
 
-    <section  class="orizontalSection">
-        <div class="quotes">
+                <p class='greetingMessageTitle'>
+                    Why do you read so much?
+                </p>
 
-            <img src="https://picsum.photos/100/100" id="personalityPortret">
-            <ul class = "nodotlist">
-                <li>
-                    <p class="quotewrt">
-                    “In three words I can sum up everything I've learned about life: it goes on.”
-                    </p>
-                </li>
-                <li>
-                    <p class="quotewrt">Robert Frost</p>
-                </li>
-            </ul>
-
-        </div>
-
-        <div class= "updates">
-            <div class="update__comp">
-                <p>Postarile colegilor: </p>
-                <p>Radu Macarof: ,,Crima din Orient Express" de Agatha Cristie</p>
-                <p>Razvan Tutuianu: ,,Crima si pedeapsa" de Fyodor Dostoyevsky</p>
-                <p>Andrei Patrascan: ,,Fizica cuantica pentru gradinita" de Arthur Smith</p>
+                <p class = 'greetingMessage'>
+                In his book How to Read and Why, Harold Bloom says that we should read slowly, with love, openness and with our inner ear cocked. He explains we should read to increase our wit and 
+                imagination, our sense of intimacy (in short, our entire consciousness) and also to heal our pain.
+                </p>
+                <p class = 'greetingMessagequote'>
+                “Until you become yourself, what benefit can you be to others.”
+                </p>
+                <p class = 'greetingMessage'>
+                With the endless amount of perspectives and lives we can read about, books can give us an opportunity to have experiences that we haven’t had the opportunity to. While still allowing 
+                us to learn the life skills they entail. Books are a fast rack to creating yourself.
+                </p>
             </div>
 
-            <div class="update__comp">
-                <p>Review-urile colegilor: </p>
-                <p>Radu Macarof(,,Crima din Orient Express" de Agatha Cristie): ,,Palpitant!"</p>
-                <p>Razvan Tutuianu(,,Crima si pedeapsa" de Fyodor Dostoyevsky): ,,Minunat scrisa"</p>
-                <p>Andrei Patrascan(,,Fizica cuantica pentru gradinita" de Arthur Smith): ,,Prea greu materialul"</p>
+            <div class = "loginElement">
             </div>
-        </div>
-    </section>
+            
+            <div class = "loginElement" id = "loginSection">
+                <p id = "segment__title">
+                    Welcome Back
+                </p>
+                <p id = "segment__message">
+                    Don't miss your next best read. Sign in to stay updated.
+                </p>
+                <button type="button" id="popup-login">Login</button>   
+                <p id = "segment__message">
+                    Not registered? Click here:
+                </p>       
+                <button type="button" id="popup-register">Register</button>
+            </div>
+        </section>
+      </section>
 
+
+      <div>
+          <p class="indentedText">
+              Computing books:  
+          <p>
+      </div>
+
+      <section class = "verticalSection">
+        <div class="contain">
+          <div class="row">
+            <div class="row__inner">
+
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000001.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000002.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000003.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000004.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000005.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000006.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000007.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000008.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000009.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Computing\0000010.jpg" alt=""  />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div>
+          <p class="indentedText">
+              Crime-Thriller books:  
+          <p>
+      </div>
+
+      <section class = "verticalSection">
+        <div class="contain">
+          <div class="row">
+            <div class="row__inner">
+
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000001.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000002.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000003.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000004.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000005.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000006.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000007.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000008.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000009.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Crime-Thriller\0000010.jpg" alt=""  />
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div>
+          <p class="indentedText">
+              Romance books:  
+          <p>
+      </div>
+
+      <section class = "verticalSection">
+        <div class="contain">
+          <div class="row">
+            <div class="row__inner">
+
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000001.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000002.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000003.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000004.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000005.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000006.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000007.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000008.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000009.jpg" alt=""  />
+              </div>
+              <div class="tile">
+                <img class="tile__img" src="bookcovers\Romance\0000010.jpg" alt=""  />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class = "verticalSection">
+        <section  class="orizontalSection">
+            <div class="statistics">      
+            <p class="statistics_text">
+                Statistics
+            </p>
+            </div>
+            <div>
+                <img src="https://picsum.photos/1700/200" class= "info">
+            </div>
+        </section>
+      </section>
+
+      <section class = "verticalSection">
+        <section  class="orizontalSection">
+            <div class="quotes">
+
+                <img src="https://picsum.photos/100/100" id="personalityPortret">
+                <ul class = "nodotlist">
+                    <li>
+                        <p class="quotewrt">
+                        “In three words I can sum up everything I've learned about life: it goes on.”
+                        </p>
+                    </li>
+                    <li>
+                        <p class="quotewrt">Robert Frost</p>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class= "updates">
+                <div class="update__comp">
+                    <p>Postarile colegilor: </p>
+                    <p>Radu Macarof: ,,Crima din Orient Express" de Agatha Cristie</p>
+                    <p>Razvan Tutuianu: ,,Crima si pedeapsa" de Fyodor Dostoyevsky</p>
+                    <p>Andrei Patrascan: ,,Fizica cuantica pentru gradinita" de Arthur Smith</p>
+                </div>
+
+                <div class="update__comp">
+                    <p>Review-urile colegilor: </p>
+                    <p>Radu Macarof(,,Crima din Orient Express" de Agatha Cristie): ,,Palpitant!"</p>
+                    <p>Razvan Tutuianu(,,Crima si pedeapsa" de Fyodor Dostoyevsky): ,,Minunat scrisa"</p>
+                    <p>Andrei Patrascan(,,Fizica cuantica pentru gradinita" de Arthur Smith): ,,Prea greu materialul"</p>
+                </div>
+            </div>
+        </section>
+      </section>
+
+    </section>
 
 </main>
 
