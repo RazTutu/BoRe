ad70bdb376218b3852e67f74bfb76c851dcddb52
AndreiPatrascan <45433079+AndreiPatrascan@users.noreply.github.com>
2020-06-10 12:41:32 +0300

    Classes with reviews

--- backend/demo2.php
+++ backend/demo2.php
@@ -68,13 +68,51 @@ if (isset($_POST['join'])) {
 <body>
   <?php include 'header.php'  ?>
   <?php
+
   $is_part_of_any_class;
   $sth = $db->prepare("SELECT * FROM group_users where username LIKE BINARY '%$username%'");
   $sth->execute();
-  $option = isset($_POST['taskOption']) ? $_POST['taskOption'] : false;
+  $option = isset($_POST['taskOption']) ? $_POST['taskOption'] : FALSE;
   $row_count_user = $sth->rowCount();
-  ///if pt submit
-  echo '<div class="second-part-with-classes">';
+
+
+  if($option != FALSE){
+      $sql = "SELECT br.book_name, br.book_author, br.book_genre, br.book_review, br.username
+      from book_reviews br join group_users gu on br.username = gu.username
+      where gu.group_name LIKE '%$option%'";
+      
+      $sth = $db->prepare($sql);
+      $sth->execute();
+      $row_count = $sth->rowCount();
+      
+
+      if ($row_count > 0) {
+        while ($row = $sth->fetch(PDO::FETCH_BOTH)) {
+            echo "<div class='article-box'>";
+            echo "<h3 class='book-name'>" . "Book : " . $row['book_name'] . "</h3>";
+            echo "<p class='book-author'>" . "Author : " . $row['book_author'] . "</p>";
+            echo "<p class='book-genre'>" . "Genre : " . $row['book_genre'] . "</p>";
+            echo "<p class='rev'> - Review - </p>";
+            $len = strlen($row['book_review']);
+            if ($len > 200) {
+                echo "<p class='review'>"  . substr($row['book_review'], 0, 200) . "<span class='dots'>...</span><span class='more'>" . substr($row['book_review'], 201, $len) . "</span>" . "</p>";
+            } else {
+                echo "<p class='review'>"  . substr($row['book_review'], 0, 200) . "<span class='dots'></span><span class='more'>" . substr($row['book_review'], 201, $len) . "</span>" . "</p>";
+            }
+            echo "<button  class='myBtn'>Read more</button>";
+            echo "<p class='review'>" . 'username: ' . $row['username'] . "</p>";
+            echo "</div>";
+        }
+      }
+      else{
+        echo "<div>";
+        echo "<h3 class='sorry'>Nu exista review-uri pentru carti in acest grup.</h3>";
+        echo "<h3 class='sorry'>Adauga un review si revino mai tarziu.</h3>";
+        echo "</div>";
+      }
+  }
+  else{
+    echo '<div class="second-part-with-classes">';
   echo ' <div class="contanter-for-text">';
   echo ' <div class="message-welcome">';
   echo  '<h1 class="question-about-classes">Nu esti parte din nicio clasa?</h1>';
@@ -141,7 +179,6 @@ if (isset($_POST['join'])) {
     echo '</div>';
     echo '<div class="opt-class">';
     echo '<form method="post" class="cob">';
-
     echo '<select class="dropdown-for-classes" name="taskOption">';
 
     while ($row = $sth->fetch(PDO::FETCH_BOTH)) {
@@ -156,7 +193,7 @@ if (isset($_POST['join'])) {
     echo '</div>';
   }
   echo '</div>';
-
+  }
   ?>
 
   </div>
