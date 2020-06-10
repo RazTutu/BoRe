0abb8a4f647e8da882a973f291e96c8dcdeccf98
radumc1 <macarof.radu@gmail.com>
2020-06-10 23:20:22 +0300

    backend validat

--- backend/chat.php
+++ backend/chat.php
@@ -2,30 +2,31 @@
 
     <h2> Chat Messages </h2>
 
-    <div class = "chat_container">
+    <div class="chat_container">
         <?php
-            $sql = "SELECT * FROM (SELECT * from group_messages order by date desc limit 30) as T order by date;";
-            $sth = $db->prepare($sql);
-            $sth->execute();
-            $row_count = $sth->rowCount();
-            
-            if ($row_count > 0) {
-                while ($row = $sth->fetch(PDO::FETCH_BOTH)) {
-                    echo "<p class='chat_user'>" . $row['username'] . ":" . $row['message']. "</p>";
-                }
+        $sql = "SELECT * FROM (SELECT * from group_messages order by date desc limit 30) as T order by date;";
+        $sth = $db->prepare($sql);
+        $sth->execute();
+        $row_count = $sth->rowCount();
+
+        if ($row_count > 0) {
+            while ($row = $sth->fetch(PDO::FETCH_BOTH)) {
+                echo "<p class='chat_user'>" . $row['username'] . ":" . $row['message'] . "</p>";
             }
+        }
         ?>
     </div>
-    
-    <form method="post" action="insert.php" class = "chatInput">
-        <label class = "chatLabel">
-            <input class = "chatCommentLabel" type="text" name="Comment" placeholder = "Scrieti un mesaj"/>
-        </label>
-        <input type="submit" class = "submitChatButton1" value="Submit"/> 
-    </form>
-    
-</div>
 
+    <form method="post" action="insert.php" class="chatInput">
+        <label class="chatLabel">
+            <input class="chatCommentLabel" type="text" name="Comment" placeholder="Scrieti un mesaj" />
+        </label>
+        <input type="submit" class="submitChatButton1" value="Submit" />
+    </form>
+
+</div>
+</div>
+</div>
 <script>
     var objDiv = document.getElementByClassName("chat_container");
     objDiv.scrollTop = objDiv.scrollHeight;
